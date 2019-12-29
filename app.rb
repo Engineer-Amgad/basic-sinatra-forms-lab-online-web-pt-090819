35bff8a8623a40500108d41ba5032b3ca69bf8b2
Amgad Malik <amgadmalik@yahoo.com>
2019-12-29 00:53:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class App < Sinatra::Base
   post '/team' do
     @team_name = params[:Team_Name]
     @coach = params[:Coach]
+    @point_guard = params[:Point_Guard]
+    @shooting_guard = params[:Shooting_Guard]
+    @small_forward = params[:Small_Forward]
+    @power_forward = params[:Power_Forward]
+    @center = params[:Center]
     
     erb :team
   end
